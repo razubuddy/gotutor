9f42b6cf37aecd7ca04940a793c329d327916715
razu <radoslaw.zubek@buddy.works>
2025-11-04 14:56:21 +0100

    asdf

--- main.go
+++ main.go
@@ -1,6 +1,40 @@
 package main
 
+import "fmt"
+
 func main() {
-	hello()
-	values()
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3 =", 7.0/3)
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c = "a", 2
+	fmt.Println(b, c)
+
+	var d int
+	fmt.Println(d)
+
+	var e string
+	fmt.Printf("'%s'\n", e)
+	fmt.Println("len =", len(e))
+
+	f := "asdf"
+	fmt.Println(f)
+
+	const n = 500000000
+	fmt.Println(n)
+
+	arr := [...]int{1, 2, 3}
+	var arr2 [3]int
+	for i := range 3 {
+		arr2[i] = i
+	}
+
+	fmt.Println(arr == arr2)
+
+	var s []string
+	fmt.Println("uniinit:", s, s == nil, len(s))
+
 }
--- values.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func values() {
-	fmt.Println("go" + "lang")
-}
