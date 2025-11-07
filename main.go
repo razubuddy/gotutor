d4ec53ee7639c87f781638eac5c2829c0d8c36d6
razu <radoslaw.zubek@buddy.works>
2025-11-07 15:40:40 +0100

    asdf

--- main.go
+++ main.go
@@ -1,40 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("go" + "lang")
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3 =", 7.0/3)
-
-	var a = "initial"
-	fmt.Println(a)
-
-	var b, c = "a", 2
-	fmt.Println(b, c)
-
-	var d int
-	fmt.Println(d)
-
-	var e string
-	fmt.Printf("'%s'\n", e)
-	fmt.Println("len =", len(e))
-
-	f := "asdf"
-	fmt.Println(f)
-
-	const n = 500000000
-	fmt.Println(n)
-
-	arr := [...]int{1, 2, 3}
-	var arr2 [3]int
-	for i := range 3 {
-		arr2[i] = i
+	var RootCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Usage()
+		},
 	}
 
-	fmt.Println(arr == arr2)
-
-	var s []string
-	fmt.Println("uniinit:", s, s == nil, len(s))
-
+	err := RootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
